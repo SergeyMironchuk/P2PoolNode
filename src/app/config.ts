3e25fe073987627c1d42ce491c4b6d9883a44f72
Sergey Mironchuk <sergey@mironchuk.in.ua>
2018-05-26 10:59:12 +0300

    Config change

--- src/app/config.ts
+++ src/app/config.ts
@@ -4,4 +4,4 @@ export const LOCAL_STATS_URL = node + '/local_stats';
 export const CURRENT_PAYOUTS_URL = node + '/current_payouts';
 export const GLOBAL_STATS_URL = node + '/global_stats';
 export const RECENT_BLOCKS_URL = node + '/recent_blocks';
-export const MINER_URL = 'stratum+tcp://prn-service.com:9327';
+export const MINER_URL = 'stratum+tcp://ltc.coinpool.pw:9327';
